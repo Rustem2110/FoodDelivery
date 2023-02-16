70315f09bcfac93648183d29980ae2ec64083dd1
Rustem2110 <98989031+Rustem2110@users.noreply.github.com>
2023-02-16 22:57:10 +0300

    Update submit-order.php

--- submit-order.php
+++ submit-order.php
@@ -12,23 +12,32 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Get form input values
-$farmer_id = $_POST['farmer_id'];
-$product_name = $_POST['product_name'];
-$quantity = $_POST['quantity'];
-$customer_name = $_POST['customer_name'];
-$customer_address = $_POST['customer_address'];
-$customer_phone = $_POST['customer_phone'];
+// Get form input values and sanitize them
+$farmer_id = mysqli_real_escape_string($conn, $_POST['farmer_id']);
+$product_name = mysqli_real_escape_string($conn, $_POST['product_name']);
+$quantity = mysqli_real_escape_string($conn, $_POST['quantity']);
+$customer_name = mysqli_real_escape_string($conn, $_POST['customer_name']);
+$customer_address = mysqli_real_escape_string($conn, $_POST['customer_address']);
+$customer_phone = mysqli_real_escape_string($conn, $_POST['customer_phone']);
 
-// Insert values into the orders table
+// Prepare the SQL statement with placeholders for the values
 $sql = "INSERT INTO orders (farmer_id, product_name, quantity, customer_name, customer_address, customer_phone) 
-VALUES ('$farmer_id', '$product_name', '$quantity', '$customer_name', '$customer_address', '$customer_phone')";
+VALUES (?, ?, ?, ?, ?, ?)";
 
-if (mysqli_query($conn, $sql)) {
+// Create a prepared statement
+$stmt = mysqli_prepare($conn, $sql);
+
+// Bind the input values to the placeholders in the statement
+mysqli_stmt_bind_param($stmt, 'ssssss', $farmer_id, $product_name, $quantity, $customer_name, $customer_address, $customer_phone);
+
+// Execute the prepared statement
+if (mysqli_stmt_execute($stmt)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
+// Close the prepared statement and the database connection
+mysqli_stmt_close($stmt);
 mysqli_close($conn);
 ?>
